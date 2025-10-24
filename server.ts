718e955bc2ce1acda1284e0343df011605a09f5b
Perry Fardella <perry.fardella@live.com>
2025-10-24 18:44:52 -0400

    Fix ts issues and get the server running properly

--- server.ts
+++ server.ts
@@ -1,1 +1,15 @@
-console.log("Hello World");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/test", (req, res) => {
+  res.send("Express is working!");
+});
+
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
