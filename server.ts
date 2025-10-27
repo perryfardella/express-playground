d05e2d85430e6b3008a8d47e6055c31cfd0daae4
Perry Fardella <perry.fardella@live.com>
2025-10-27 18:59:23 -0400

    Install dotenv and use env vars in server.ts

--- server.ts
+++ server.ts
@@ -1,10 +1,13 @@
+import dotenv from "dotenv";
 import app from "./app";
+
+// Load environment variables from .env file
+dotenv.config();
 
 // This file contains all the code that only runs once (on server start).
 // Keeping the startup logic separate from the business logic (routes).
 
-const PORT = 4000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on  http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on  http://localhost:${process.env.PORT}`);
+  console.log(`Server environment: ${process.env.NODE_ENV}`);
 });
