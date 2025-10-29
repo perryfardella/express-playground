74c1fa0d599ad0ed7d3f5030504f33ed27d2de5b
Perry Fardella <perry.fardella@live.com>
2025-10-29 11:11:04 -0400

    Add Mongoose and db connection logic

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import app from "./app";
+import { connectDB } from "./config/db";
 
 // Load environment variables from .env file
 dotenv.config();
@@ -7,7 +8,20 @@ dotenv.config();
 // This file contains all the code that only runs once (on server start).
 // Keeping the startup logic separate from the business logic (routes).
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on  http://localhost:${process.env.PORT}`);
-  console.log(`Server environment: ${process.env.NODE_ENV}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB(); // Connect to MongoDB before starting the server
+
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `✅ Server is running on http://localhost:${process.env.PORT}`
+      );
+      console.log(`🌐 Server environment: ${process.env.NODE_ENV}`);
+    });
+  } catch (error) {
+    console.error("❌ Failed to start server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
