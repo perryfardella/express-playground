bb884f43d57af730fc5cae8c0ccdf284793878e8
Perry Fardella <perry.fardella@live.com>
2025-10-30 15:58:10 -0400

    Add cors and morgan middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import tasksRouter from "./routes/tasks.js";
 import { errorHandler } from "./middleware/errorHandler.js";
+import morgan from "morgan";
+import cors from "cors";
 
 // This file creates teh express instance and exports it.
 // In the future we will also apply global middleware here.
@@ -8,8 +10,13 @@ import { errorHandler } from "./middleware/errorHandler.js";
 
 const app: express.Application = express();
 
+// Core middleware
 // This middleware is used to parse incoming request bodies in a JSON format into javascript objects.
 app.use(express.json());
+// This middleware is used to enable CORS.
+app.use(cors()); // TODO: Configure this more strictly in prod
+// This middleware is used to log incoming requests to the console.
+app.use(morgan("dev")); // TODO: Consider using combined logging in prod
 
 // Mount route modules
 app.use("/tasks", tasksRouter);
